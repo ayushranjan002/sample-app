11ba7996420dd4edf3bee651297298675080d6d8
Ayush Ranjan <ayushpcs02@gmail.com>
2025-09-09 01:45:45 +0530

    Initial sample app

---
+++ app.js
@@ -0,0 +1,10 @@
+// app.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from sample app!');
+});
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
